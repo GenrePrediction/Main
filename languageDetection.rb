63f3a9d29141f8c03c4da4a81cfcf1df62951db3
Michal Snoch <kontakt@msnoch.pl>
2012-09-30 12:16:18 +0200

    language detection script

---
+++ languageDetection.rb
@@ -0,0 +1,17 @@
+require 'sqlite3'
+require 'wtf_lang'
+
+WtfLang::API.key = '2310f653efdebf2aee5c62c425706206'
+
+db = SQLite3::Database.new('lyrics.db')
+db.results_as_hash = true
+
+
+db.execute('select * from lyrics where lang is null') do |row|
+  print 'Checking ', row['author'], ' - ', row['song'], ': '
+  vl = row['lyrics'].lang
+  puts vl
+  db.execute("update lyrics set lang='#{vl}' where id=#{row['id']}")
+end
+
+db.close
